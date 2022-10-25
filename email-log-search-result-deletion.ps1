3ec43a25a378b845cf18dba572be4287adc7d2cd
Kaden Scroggins <105025952+kadenscroggins@users.noreply.github.com>
2022-10-25 10:50:24 -0500

    Add files via upload

---
+++ email-log-search-result-deletion.ps1
@@ -0,0 +1,13 @@
+# Can be used to get the deletion commands for a lot of messages with different message IDs
+# if they are all in the same CSV file from a Google Admin Email Log Search CSV export
+
+[string[]]$messages = Get-Content -Path ".\LogSearchResults.csv"
+Write-Output $messages[0]
+foreach ($line in $messages) {
+    $columns = $line.Split(",")
+    $command = ("gam user " + $columns[8].Replace("`"","") + " delete messages query rfc822msgid:" + $columns[0].Replace("`"","") + " doit")
+    
+    # Uncomment the second line to run the commands
+    Write-Output $command
+    #Invoke-Expression -Command $command
+}
