7c061a8da846920c1076bb19ac0531f1d9ca3e36
Kaden Scroggins <105025952+kadenscroggins@users.noreply.github.com>
2023-08-29 14:52:05 -0500

    Update email-log-search-result-deletion.ps1
    Updated to use multiple threads and filter out users on different domains

--- email-log-search-result-deletion.ps1
+++ email-log-search-result-deletion.ps1
@@ -1,12 +1,19 @@
 # Can be used to get the deletion commands for a lot of messages with different message IDs
 # if they are all in the same CSV file from a Google Admin Email Log Search CSV export
-
-[string[]]$messages = Get-Content -Path ".\LogSearchResults.csv"
+$batch_path = '.\gam-batch-log-search-results-deletion.txt'
+[System.Collections.ArrayList]$messages = Get-Content -Path ".\LogSearchResults.csv"
+Write-Output "Removing first line: $($messages[0])"
+$messages.RemoveAt(0)
+$command_set = New-Object System.Collections.Generic.HashSet[String]
 foreach ($line in $messages) {
     $columns = $line.Split(",")
     $command = ("gam user " + $columns[8].Replace("`"","") + " delete messages query rfc822msgid:" + $columns[0].Replace("`"","") + " doit")
-    
-    # Uncomment the second line to run the commands
-    Write-Output $command
-    #Invoke-Expression -Command $command
+    if ($columns[8] -like "*exampledomain.com*") {
+        $command_set.Add($command) | Out-Null
+    }
 }
+
+$command_set | out-file $batch_path -Encoding UTF8
+Write-Host "Deleting $($command_set.Count) emails"
+gam batch $batch_path
+Remove-Item $batch_path
